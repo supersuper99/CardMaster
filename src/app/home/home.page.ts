b6d414ef1adf1ec0394c310b27e954fbb1ec5c6b
PAPI <Eandrews100@gmail.com>
2022-12-26 15:40:56 -0800

    functions

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +8,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   currentCard: string = 'Ace of Spades';
+  score: number = 0;
+  cardHistory: any[] = [];
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
 
   addOne() {
-    
+    // Implement logic to add one to the current card count and update the score
+    this.score++;
+    this.cardHistory.push({
+      name: this.currentCard,
+      result: '+1',
+    });
   }
 
   subtractOne() {
-   
+    // Implement logic to subtract one from the current card count and update the score
+    this.score--;
+    this.cardHistory.push({
+      name: this.currentCard,
+      result: '-1',
+    });
+  }
+
+  openTutorial() {
+    // Navigate to the tutorial page
+    this.navCtrl.navigateForward('/tutorial');
+  }
+
+  openSettings() {
+    // Navigate to the settings page
+    this.navCtrl.navigateForward('/settings');
   }
 }
